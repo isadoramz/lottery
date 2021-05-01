0978100179debd62257e3826ba2b044126c88868
Isadora Zandonotto <isadoraimz@gmail.com>
2021-05-01 16:42:01 -0300

    refactor: payment message

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -1,14 +1,16 @@
 
-import {  useContext } from "react"
-import { LotteryContext } from "../../contexts/LotteryContext"
+import {  useContext } from "react";
+import { LotteryContext } from "../../contexts/LotteryContext";
 
 const Payment = () => {
-    const { gamePrice } = useContext(LotteryContext);
-    console.log(gamePrice)
+	const { gamePrice } = useContext(LotteryContext);
 
-    return (
-        <p>pagamento {gamePrice}</p>
-    )
+	return (
+		<div>
+			<p>Você finalizou o jogo!</p>
+			<p>Valor total R${gamePrice}</p>
+		</div>
+	)
 }
 
 export default Payment;
