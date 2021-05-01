5a0a4b5e1ef73349e6debb51cd2e6ef1a97cac9e
Isadora Zandonotto <isadoraimz@gmail.com>
2021-05-01 16:39:16 -0300

    feat: add style to LotteryInfo

--- src/components/LotteryInfo/index.js
+++ src/components/LotteryInfo/index.js
@@ -1,38 +1,43 @@
-import { useContext } from "react"
-import { Link } from "react-router-dom"
-import { LotteryContext } from "../../contexts/LotteryContext.js"
+import { useContext } from "react";
+import { LotteryContext } from "../../contexts/LotteryContext.js";
+import { useHistory } from "react-router-dom"
+
+import "./styles.css"
 
 const OptionText = ({ options, optionsToRender, price, type}) => {
-    const { setGamePrice, gamePrice } = useContext(LotteryContext);
+	let history = useHistory();
 
-    if(options.length === 0) {
-        return <p>Você ainda nao marcou nenhuma dezena</p>
-    } else if(options.length === 1) {
-        return <p>Você selecionou a dezena: {optionsToRender}</p>
-    } else if(options.length > 1) {
-        if(type === "sena") {
-            if(options.length >= 6) {
-                return (
-                    <>
-                        <p>Você selecionou as dezenas: {optionsToRender}</p>
-                        <p>valor jogo: {price(options, "sena")} reais</p>
-                        <button onClick={() => setGamePrice(price(options, "sena"))}><Link to="/pagamento">Confirmar Jogo</Link></button>
-                    </>
-                )
-            }
-        } else if(type === "quina") {
-            if(options.length >= 5) {
-                return (
-                    <>
-                        <p>Você selecionou as dezenas: {optionsToRender}</p>
-                        <p>valor jogo: {price(options, "quina")} reais</p>
-                        <button onClick={() => setGamePrice(price(options, "quina"))}><Link to="/pagamento">Confirmar Jogo</Link></button>
-                    </>
-                )
-            }
-        }
-        
-        return <p>Você selecionou as dezenas: {optionsToRender}</p>
+	const { setGamePrice } = useContext(LotteryContext);
+
+	if(options.length === 0) {
+		return <p className="text">Você ainda não marcou nenhuma dezena!</p>
+	} else if(options.length >= 1) {
+			if(type === "sena") {
+				if(options.length >= 6) {
+					return (
+						<>
+							<p className="text">Você selecionou a(s) dezena(s): {optionsToRender}</p>
+							<div className="priceWrapper">
+								<p className="text">Valor do jogo: R${price(options, "sena")}</p>
+								<button tyoe="button" className="button" onClick={() => {setGamePrice(price(options, "sena"), history.push("/pagamento"))}}>Confirmar jogo</button>
+							</div>
+						</>
+					)
+				}
+			} else if(type === "quina") {
+					if(options.length >= 5) {
+						return (
+							<>
+								<p className="text">Você selecionou a(s) dezena(s): {optionsToRender}</p>
+								<div className="priceWrapper">
+									<p className="text">Valor do jogo: R${price(options, "quina")}</p>
+									<button tyoe="button" className="button" onClick={() => {setGamePrice(price(options, "quina"), history.push("/pagamento"))}}>Confirmar jogo</button>
+								</div>
+							</>
+						)
+					}
+			}
+			return <p className="text">Você selecionou as dezenas: {optionsToRender}</p>
     }
 }
 
