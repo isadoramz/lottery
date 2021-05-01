7f92b43f34da4257bd1b8a0ebd5121726e1fc08f
Isadora Zandonotto <isadoraimz@gmail.com>
2021-05-01 16:40:03 -0300

    refactor: lottery context

--- src/contexts/LotteryContext.js
+++ src/contexts/LotteryContext.js
@@ -3,14 +3,14 @@ import React, { useState, createContext, useEffect } from "react";
 export const LotteryContext = createContext();
 
 const LotteryProvider = ({ children }) => {
-    const [currentTab, setCurrentTab] = useState("sena")
-    const [gamePrice, setGamePrice] = useState()
+	const [currentTab, setCurrentTab] = useState("sena");
+	const [gamePrice, setGamePrice] = useState();
 
-    return (
-        <LotteryContext.Provider value={{currentTab, setCurrentTab, gamePrice, setGamePrice}}>
-            {children}
-        </LotteryContext.Provider>
-    )
+	return (
+		<LotteryContext.Provider value={{currentTab, setCurrentTab, gamePrice, setGamePrice}}>
+			{children}
+		</LotteryContext.Provider>
+	)
 }
 
 export default LotteryProvider;
