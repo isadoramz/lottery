b9b371f8fbd32570b4cfedb44885420755558176
Isadora Zandonotto <isadoraimz@gmail.com>
2021-05-01 20:47:22 -0300

    refactor: LotteryInfo

--- src/components/LotteryInfo/index.js
+++ src/components/LotteryInfo/index.js
@@ -11,34 +11,49 @@ const OptionText = ({ options, optionsToRender, price, type}) => {
 
 	if(options.length === 0) {
 		return <p className="text">Você ainda não marcou nenhuma dezena!</p>
-	} else if(options.length >= 1) {
-			if(type === "sena") {
-				if(options.length >= 6) {
-					return (
-						<>
-							<p className="text">Você selecionou a(s) dezena(s): {optionsToRender}</p>
-							<div className="priceWrapper">
-								<p className="text">Valor do jogo: R${price(options, "sena")}</p>
-								<button tyoe="button" className="button" onClick={() => {setGamePrice(price(options, "sena"), history.push("/pagamento"))}}>Confirmar jogo</button>
-							</div>
-						</>
-					)
-				}
-			} else if(type === "quina") {
-					if(options.length >= 5) {
-						return (
-							<>
-								<p className="text">Você selecionou a(s) dezena(s): {optionsToRender}</p>
-								<div className="priceWrapper">
-									<p className="text">Valor do jogo: R${price(options, "quina")}</p>
-									<button tyoe="button" className="button" onClick={() => {setGamePrice(price(options, "quina"), history.push("/pagamento"))}}>Confirmar jogo</button>
-								</div>
-							</>
-						)
-					}
+	} 
+	
+	if(type === "sena") {
+		if(options.length >= 1 && options.length < 6) {
+			return (
+				<div className="priceWrapper">
+					<p className="text">Você selecionou a(s) dezena(s):</p>
+					<p className="text">{optionsToRender}</p>
+				</div>
+			)
+		} 
+		if(options.length >= 6) {
+			return (
+				<>
+					<p className="text">Você selecionou a(s) dezena(s): {optionsToRender}</p>
+					<div className="priceWrapper">
+						<p className="text">Valor do jogo: R${price(options, "sena")}</p>
+						<button tyoe="button" className="button" onClick={() => {setGamePrice(price(options, "sena"), history.push("/pagamento"))}}>Confirmar jogo</button>
+					</div>
+				</>
+			)
+		} 
+	} else if(type === "quina") {
+			if(options.length >= 1 && options.length < 5) {
+				return (
+					<div className="priceWrapper">
+						<p className="text">Você selecionou a(s) dezena(s):</p>
+						<p className="text">{optionsToRender}</p>
+					</div>
+				)
 			}
-			return <p className="text">Você selecionou as dezenas: {optionsToRender}</p>
-    }
+			if(options.length >= 5) {
+				return (
+					<>
+						<p className="text">Você selecionou a(s) dezena(s): {optionsToRender}</p>
+						<div className="priceWrapper">
+							<p className="text">Valor do jogo: R${price(options, "quina")}</p>
+							<button tyoe="button" className="button" onClick={() => {setGamePrice(price(options, "quina"), history.push("/pagamento"))}}>Confirmar jogo</button>
+						</div>
+					</>
+				)
+			}
+		}
 }
 
 export default OptionText;
