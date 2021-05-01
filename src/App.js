a4b32b42dae0f3b5c2374569e24f1c9ca64ef4d7
Isadora Zandonotto <isadoraimz@gmail.com>
2021-05-01 16:37:49 -0300

    refactor: general style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
 import LotteryProvider from './contexts/LotteryContext';
 import Routes from "./routes"
 
 function App() {
   return (
-    <div className="App">
       <LotteryProvider>
         <Routes/>
       </LotteryProvider>
-    </div>
   );
 }
 
